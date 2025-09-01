3baf253d96fb7f4c98d712a25948afcc8cc9104b
diego082006 <ddelgado1@mail.austral.edu.ar>
2025-09-01 13:12:06 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,8 +1,13 @@
-def slice_simple():
-    texto = "Awesome"
-    # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
-    # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
+def slice_advanced(texto):
+    resultado = texto[4::2] # Obtener el texto a partir del quinto carácter y saltar de a 2 caracteres
+    return resultado
 
+def main():
+    texto = input("Ingrese un texto: ") # Ingresar un texto usando input
+    
+    # Imprimir en pantalla el resultado
+    print(slice_advanced(texto))
+    # Código a implementar utilizando input.
 
-# Para verificar este ejercicio ejecutar el comando
-# `pytest tp3_slice_simple_test.py` o `python tp3_slice_simple_test.py`
+if __name__ == "__main__":
+    main()
