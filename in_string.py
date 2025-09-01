843026772a64ed85abb40164f78a8b95abca4d2e
Diego Baldassare <diegobaldassare@gmail.com>
2025-09-01 12:40:39 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,6 +1,18 @@
 def check_vowels():
     # Código a implementar utilizando input.
 
+    vocales = 'aeiou'
+    nombre = nombre.lower()  
+    for vocal in vocales:
+        print(f"Contiene {vocal}: {vocal in nombre}")
+
+# Verificación de cada una de sus vocales
+def main():
+    nombre = input("Ingrese un nombre: ")
+    verificar_vocales(nombre)
+
+if __name__ == "__main__":
+    main()
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_in_string_test.py` o `python tp3_in_string_test.py`
