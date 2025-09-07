e20a401ca01ce033be90f27751b26d822fafb221
diego082006 <ddelgado1@mail.austral.edu.ar>
2025-09-07 19:34:59 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,15 +1,18 @@
-def slice_advanced():
-    resultado = texto[4::2] # Obtener el texto a partir del quinto carácter y saltar de a 2 caracteres
-    return resultado
+def test_slice_advanced():
+    texto = "Awesome"
 
-def main():
-    texto = input("Ingrese un texto: ") # Ingresar un texto usando input
-    
-    # Imprimir en pantalla el resultado
-    print(slice_advanced(texto))
-    # Código a implementar utilizando input.
+    # Pasar todo a minúsculas
+    texto = texto.lower()
 
-# Para verificar este ejercicio ejecutar el comando
-# `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
-if __name__ == "__main__":
-    main()
+    # Primeras 3 letras (índices positivos)
+    print(texto[0:3])
+
+    # 3 letras del medio (índices positivos)
+    medio = len(texto) // 2
+    print(texto[medio-1:medio+2])
+
+    # De la primera a la cuarta letra (incluida)
+    # y de la antepenúltima hasta la última (incluida)
+    print(texto[0:4] + texto[-3:])
+
+test_slice_advanced()
