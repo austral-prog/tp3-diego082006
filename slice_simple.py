9b9104a1d7559b28ddf72bfec22090abbbb40671
diego082006 <ddelgado1@mail.austral.edu.ar>
2025-09-07 15:26:17 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,16 +1,12 @@
-texto = "AWESOME".lower()
+def slice_advanced(texto):
+    resultado = texto[4::2]  # Obtener el texto a partir del quinto carácter y saltar de a 2
+    return resultado
 
-# Primeras 3 letras del texto
-primeras_tres_letras = texto[:3]
-print(primeras_tres_letras)
+def main():
+    texto = input("Ingrese un texto: ")  # Ingresar un texto usando input
+    
+    # Imprimir en pantalla el resultado
+    print(slice_advanced(texto))
 
-# Las 3 letras en medio del texto
-longitud = len(texto)
-mitad = longitud//2
-tres_letras_medio = texto[mitad-1:mitad+2]
-print(tres_letras_medio)
-
-# De la primera a la cuarta letra y de la antepenúltima hasta la última
-primera_a_cuarta = texto[:4]
-antepenultima_a_ultima = texto[-3:]
-print(primera_a_cuarta + antepenultima_a_ultima)
+if __name__ == "__main__":
+    main()
