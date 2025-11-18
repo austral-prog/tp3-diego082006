f9f49aeaa2848ad0f45616f4429a5270c7dfb6a6
diego082006 <ddelgado1@mail.austral.edu.ar>
2025-11-18 19:34:18 -0300

    Fix formatting and ensure newline at end of file

--- slice_simple.py
+++ slice_simple.py
@@ -1,11 +1,9 @@
 def slice_simple(texto):
-    resultado = texto[4::2]  # Obtener el texto a partir del quinto carácter y saltar de a 2
+    resultado = texto[4::2]  
     return resultado
 
 def main():
-    texto = input("Ingrese un texto: ")  # Ingresar un texto usando input
-    
-    # Imprimir en pantalla el resultado
+    texto = input("Ingrese un texto: ") 
     print(slice_simple(texto))
 
 main()
