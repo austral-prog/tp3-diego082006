753ba460f892c5acaa7641b7a7cda19bddb35168
diego082006 <ddelgado1@mail.austral.edu.ar>
2025-11-18 19:44:12 -0300

    Refactor test function to slice_advanced function

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,6 +1,12 @@
-def test_slice_advanced():
+def slice_advanced():
     texto = "Awesome"
     texto = texto.lower()
+    largo = len(texto)
+    medio = largo // 2
+
+    # Tres Salidas
     print(texto[0:3])
     print(texto[medio-1:medio+2])
     print(texto[0:4] + texto[-3:])
+
+slice_advanced()
