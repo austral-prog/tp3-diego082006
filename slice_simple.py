4985d531ea2b2d0eee62d4a1a137aa0bac63f244
diego082006 <ddelgado1@mail.austral.edu.ar>
2025-11-18 19:51:04 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,9 +1,3 @@
 def slice_simple(texto):
-    resultado = texto[4::2]  
+    resultado = texto[4::2]
     return resultado
-
-def main():
-    texto = input("Ingrese un texto: ") 
-    print(slice_simple(texto))
-
-main()
