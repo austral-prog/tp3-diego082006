b61b3570b865b3400499f0a4a80e8def60de0501
diego082006 <ddelgado1@mail.austral.edu.ar>
2025-11-18 20:23:53 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,9 +1,4 @@
 def slice_simple():
-    # 1. Get input (this is mocked by the test file)
     texto = input("Ingrese un texto: ") 
-    
-    # 2. Perform the slicing: Start at index 4 (5th character), step by 2
     resultado = texto[4::2]
-    
-    # 3. Print the result
     print(resultado)
