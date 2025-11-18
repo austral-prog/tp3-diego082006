0b5a57776c9fb86fbf6643101ba8938bfede38d0
diego082006 <ddelgado1@mail.austral.edu.ar>
2025-11-18 20:22:13 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -2,13 +2,7 @@ def slice_advanced():
     texto = "Awesome"
     texto = texto.lower()
     largo = len(texto)
-    medio = largo // 2  # Define medio here
-
-    # Primeras 3 letras (awe)
+    medio = largo // 2
     print(texto[0:3])
-
-    # Las 3 letras en medio (eso)
     print(texto[medio-1:medio+2])
-
-    # De la primera a la cuarta + antepenúltima hasta la última (awesome)
     print(texto[0:4] + texto[-3:])
