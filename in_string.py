a79ae9c027fa97e3c21d4e6d89597e6ae1e35f78
diego082006 <ddelgado1@mail.austral.edu.ar>
2025-11-19 08:57:17 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,6 +1,5 @@
 def check_vowels():
-    nombre = input("Ingrese un nombre: ").lower()
+    nombre = input().lower()
     vocales = ["a", "e", "i", "o", "u"]
-
     for vocal in vocales:
         print(f"Contiene {vocal}: {vocal in nombre}")
